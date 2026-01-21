0c0ec6bb30f86eb284eee52b1fd96f5f68e158ca
Koffertje <trwn87@gmail.com>
2026-01-21 11:09:44 +0100

    Update count.js

--- count.js
+++ count.js
@@ -1,2 +1,5 @@
 millis = Date.now() - (new Date((new Date(Date.now())).getFullYear(), 0, 1));
-document.querySelector("#count").innerHTML = Math.floor(720000 * (millis / 31556952000));
+count = 720000 * (millis / 31556952000);
+rpc = 8300000000 / count;
+document.querySelector("#count").innerHTML = Math.floor(count);
+document.querySelector("#rpc").innerHTML = Math.round(rpc);
